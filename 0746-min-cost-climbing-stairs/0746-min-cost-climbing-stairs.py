aa49d411e5ebaeb8f3670dc584fe672bef390bf2
Tek Chand Bareja <105941984+tekchandbadeja@users.noreply.github.com>
2022-11-25 21:52:28 +0530

    Time: 144 ms (20.10%), Space: 15 MB (15.35%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,6 +1,6 @@
 def solve(cost,i,dp):
         
-        if i==len(cost)-1 or i==len(cost)-2:
+        if i==len(cost)-1 or i==len(cost)-2:    ### bhai mai 1 step jump kar skta hu ya do ab yadi cost me 1 elemnt ya do bache hai to mai puncgh jaunga na usko leke 
                 return cost[i]
         if dp[i]!=-1:
                 return dp[i]
@@ -17,7 +17,7 @@ class Solution:
         if n==2:
                 return min(cost)
         dp=[-1 for i in range (n+1)]
-        output1=solve(cost,0,dp)
-        output2=solve(cost,1,dp)
-        
+        output1=solve(cost,0,dp)          ##3 mai 0 se start kar skta hu 
+        output2=solve(cost,1,dp)    ### mai 1 se start kar skta hu 
+         ###3 bhai mere ko 0 index se start ho ya 1 index dono me se minium chiyeee esliye finally minium liya 
         return  min(output1,output2)
