2a478a276cd7fdb6ee5d8019e606e8cc93c2e808
Tek Chand Bareja <105941984+tekchandbadeja@users.noreply.github.com>
2022-11-26 10:30:17 +0530

    Time: 24 ms (99.38%), Space: 13.8 MB (55.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,20 +1,10 @@
 def fib_helper(n,dp):
         if n==0 or n==1:
                 return n
-        if dp[n-1]==-1:
-                ans1=fib_helper(n-1,dp)
-                dp[n-1]=ans1
-                
-        else:
-                ans1=dp[n-1]
-                
-        if dp[n-2]==-1:
-                ans2=fib_helper(n-2,dp)
-                dp[n-2]=ans2
-        else:
-                ans2=dp[n-2]
-                
-        return ans1+ans2
+        if dp[n]!=-1:
+                return dp[n]
+        dp[n]=fib_helper(n-1,dp)+fib_helper(n-2,dp)
+        return dp[n]
 class Solution:
     def fib(self, n: int) -> int:
         dp=[-1 for i in range (n+1)]
