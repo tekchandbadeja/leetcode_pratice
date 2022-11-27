654dd666dfdacd5333aae8836327f75987acf3a8
Tek Chand Bareja <105941984+tekchandbadeja@users.noreply.github.com>
2022-11-27 21:43:32 +0530

    Time: 45 ms (75.91%), Space: 14 MB (19.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,22 +1,34 @@
-def helper(index,nums,dp):
-        
-        ### base case 
-        if index==0:
-                
-                 #### ye tabhee possible hai jab me pichle 2 index se aaya hu to ye mere ko lena padega 
-                return nums[0]
-        
-        if index<0:
+def helper_new(index,nums,dp):
+        if index>=len(nums):
                 return 0
+        
         if dp[index]!=-1:
                 return dp[index]
-        pick=nums[index]+helper(index-2,nums,dp)
         
-        not_pick=0+helper(index-1,nums,dp)
+        pick=nums[index]+helper_new(index+2,nums,dp)
         
+        not_pick=0+helper_new(index+1,nums,dp)
         dp[index]=max(pick,not_pick)
-        
         return dp[index]
+# def helper(index,nums,dp):
+        
+#         ### base case 
+#         if index==0:
+                
+#                  #### ye tabhee possible hai jab me pichle 2 index se aaya hu to ye mere ko lena padega 
+#                 return nums[0]
+        
+#         if index<0:
+#                 return 0
+#         if dp[index]!=-1:
+#                 return dp[index]
+#         pick=nums[index]+helper(index-2,nums,dp)
+        
+#         not_pick=0+helper(index-1,nums,dp)
+        
+#         dp[index]=max(pick,not_pick)
+        
+#         return dp[index]
                 
                 
 class Solution:
@@ -30,5 +42,7 @@ class Solution:
         
         dp=[-1 for i in range (n+1)]
         
-        return helper(n-1,nums,dp)
+        # return helper(n-1,nums,dp)
+        
+        return helper_new(0,nums,dp)
         
