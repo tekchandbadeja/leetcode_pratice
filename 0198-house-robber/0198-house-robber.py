ca8d0db66a784907e50fa1c38c4e772ee1ea4f87
Tek Chand Bareja <105941984+tekchandbadeja@users.noreply.github.com>
2022-11-27 21:31:07 +0530

    Time: 67 ms (21.45%), Space: 13.9 MB (66.34%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,34 @@
+def helper(index,nums,dp):
+        
+        ### base case 
+        if index==0:
+                
+                 #### ye tabhee possible hai jab me pichle 2 index se aaya hu to ye mere ko lena padega 
+                return nums[0]
+        
+        if index<0:
+                return 0
+        if dp[index]!=-1:
+                return dp[index]
+        pick=nums[index]+helper(index-2,nums,dp)
+        
+        not_pick=0+helper(index-1,nums,dp)
+        
+        dp[index]=max(pick,not_pick)
+        
+        return dp[index]
+                
+                
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        n=len(nums)
+        
+        #### mere ko 0 se last indx tak ka maxium sum chiyeee 
+        
+        ### so i am giving from last indx 
+        
+        dp=[-1 for i in range (n+1)]
+        
+        return helper(n-1,nums,dp)
+        
