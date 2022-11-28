e34340aba3f69d263dfd959a17b78538ea7d14ce
Tek Chand Bareja <105941984+tekchandbadeja@users.noreply.github.com>
2022-11-28 07:09:22 +0530

    Time: 61 ms (43.02%), Space: 13.9 MB (66.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,15 @@
-def helper_new(index,nums,dp):
-        if index>=len(nums):
-                return 0
+# def helper_new(index,nums,dp):
+#         if index>=len(nums):
+#                 return 0
         
-        if dp[index]!=-1:
-                return dp[index]
+#         if dp[index]!=-1:
+#                 return dp[index]
         
-        pick=nums[index]+helper_new(index+2,nums,dp)
+#         pick=nums[index]+helper_new(index+2,nums,dp)
         
-        not_pick=0+helper_new(index+1,nums,dp)
-        dp[index]=max(pick,not_pick)
-        return dp[index]
+#         not_pick=0+helper_new(index+1,nums,dp)
+#         dp[index]=max(pick,not_pick)
+#         return dp[index]
 # def helper(index,nums,dp):
         
 #         ### base case 
@@ -34,15 +34,49 @@ def helper_new(index,nums,dp):
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        
+        ### now with space optimization 
+        
         n=len(nums)
         
-        #### mere ko 0 se last indx tak ka maxium sum chiyeee 
+        if n==0:
+                return 0
+        if n==1:
+                return nums[0]
         
-        ### so i am giving from last indx 
+        value1=nums[0]
         
-        dp=[-1 for i in range (n+1)]
+        value2=max(nums[0],nums[1])
         
-        # return helper(n-1,nums,dp)
+        if n==2:
+                return value2
+        max_value=float('-inf')
         
-        return helper_new(0,nums,dp)
+        for i in range (2,n):
+                
+                max_value=max(nums[i]+value1,0+value2)
+                
+                value1=value2
+                value2=max_value
+                
+        return max_value 
+        
+        
+#         n=len(nums)
+        
+#         #### mere ko 0 se last indx tak ka maxium sum chiyeee 
+        
+#         ### so i am giving from last indx 
+        
+#         dp=[-1 for i in range (n+1)]
+        
+#         # return helper(n-1,nums,dp)
+        
+#         return helper_new(0,nums,dp)
+          
+         
+     
+
+
+
         
