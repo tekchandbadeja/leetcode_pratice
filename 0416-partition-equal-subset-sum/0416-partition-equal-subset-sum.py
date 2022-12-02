bfe6ef63683974fd046270a3ae5b12613ee45c6a
Tek Chand Bareja <105941984+tekchandbadeja@users.noreply.github.com>
2022-12-02 14:12:48 +0530

    Time: 4203 ms (29.21%), Space: 30.4 MB (30.65%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,21 +1,57 @@
-def helper(index,arr,target,dp):
+# def helper(index,arr,target,dp):
         
-        if target==0:
-                return True 
-        if index==0:
-                return  arr[0]==target
+#         if target==0:
+#                 return True 
+#         if index==0:
+#                 return  arr[0]==target
         
-        if dp[index][target]!=-1:
-                return dp[index][target]
+#         if dp[index][target]!=-1:
+#                 return dp[index][target]
         
-        not_take=helper(index-1,arr,target,dp)
+#         not_take=helper(index-1,arr,target,dp)
         
-        take=False 
+#         take=False 
+#         if arr[index]<=target:
+#                 take=helper(index-1,arr,target-arr[index],dp)
         
-        take=helper(index-1,arr,target-arr[index],dp)
+#         dp[index][target]=(not_take or take)
+#         return dp[index][target]
+def isSubsetSum (N, arr, sum):
         
-        dp[index][target]=(not_take or take)
-        return dp[index][target]
+
+        dp=[[0 for i in range (sum+1)] for j in range (N)]  
+        ### abhi sab jgh 0 hai 
+        
+        ## first base case 
+        
+        for i in range (N):
+            
+            dp[i][0]=True 
+        
+        ### second base case 
+        if arr[0]<=sum:
+                dp[0][arr[0]]=True 
+        
+        
+        for i in range (1,N):
+            
+            for target in range (1,sum+1):
+                
+                not_take=dp[i-1][target]
+                
+                take=False 
+                
+                if arr[i]<=target:
+                    
+                    take=dp[i-1][target-arr[i]]
+                    
+                dp[i][target]=take or not_take 
+                
+        return dp[N-1][sum]
+                        
+                
+        
+        
                 
         
 class Solution:
@@ -33,11 +69,13 @@ class Solution:
                 return False 
         else:
                 target=sum//2
-                dp=[[-1 for i in range (target+1)] for j in range (n)]
-                return helper(n-1,nums,target,dp)
+                #dp=[[-1 for i in range (target+1)] for j in range (n)]
+                return isSubsetSum (n,nums,target)
                 
         
 #  '''      
 #         bhai mere ko ek array ko do equal sum vaale part me divide karna hai bhai ek baat btaoo yadi 
 #         array ka sum dd hai maan lo 21 hai than how can you divide it in two eual part , i say no , not possiblr , so by this I found a small conclusion that if my array sum is even than only i can divide it two eual sum 
 # ''' 
+
+# we will prefer tabulation 
