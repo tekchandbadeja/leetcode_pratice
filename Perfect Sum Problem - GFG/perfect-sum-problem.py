31c8c4c01e2d456ad09dea21ee6ab400541e2e9a
Tek Chand Bareja <105941984+tekchandbadeja@users.noreply.github.com>
2022-12-11 10:00:17 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.py
+++ Perfect Sum Problem - GFG/perfect-sum-problem.py
@@ -29,10 +29,36 @@ def helper(arr,target,index,dp):
         
     dp[index][target]= ((pick%m)+(not_pick%m))%m
     return dp[index][target]
+
 class Solution:
 	def perfectSum(self, arr, n, sum):
 		# code here
+# 		target=sum
+		m=(10**9)+7
 		dp=[[-1 for i in range (sum+1)] for j in range (n)]
+		
+# 		if arr[0]==0 and sum==0:
+# 		    dp[0][0]=2
+		    
+# 		if arr[0]!=0 and arr[0]<=sum:
+# 		    dp[0][arr[0]]=1
+# 		else:
+# 		    dp[0][0]=1
+		    
+		    
+		
+		    
+# 		for index in range (1,n):
+# 		    for target in range (1,sum+1):
+		        
+# 		        not_pick=dp[index-1][target]
+# 		        pick=0
+# 		        if arr[index]<=target:
+# 		            pick=dp[index-1][target-arr[index]]
+# 		        dp[index][target]= ((pick%m)+(not_pick%m))%m
+		        
+#         return dp[n-1][sum]
+		        
 		return helper(arr,sum,n-1,dp)
 
 
