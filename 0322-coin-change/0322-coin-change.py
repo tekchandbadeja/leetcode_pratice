e8ca8848e0c7b62f4de0eaa45c17e722f81bd945
Tek Chand Bareja <105941984+tekchandbadeja@users.noreply.github.com>
2022-12-12 18:01:43 +0530

    Time: 3631 ms (17.27%), Space: 18.9 MB (15.45%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,70 +1,41 @@
-# def solve_recursion(coins,amount):
-#         ### base case 
-#         if amount==0:
-#                 return 0
-#         if amount<0:
-#                 return float('inf')
+def helper(coins,index,amount,dp):
         
-#         #### we want minium number of coins so i am taking starting minium as highst value and update it 
-#         mini=float('inf')
+        if index==0:
+             if amount%(coins[index])==0:
+                return amount//(coins[index])
+             
+             else:
+                return (10**9)+7
+        if dp[index][amount]!=-1:
+                return dp[index][amount]
         
-#         #### saare coins per jaana hai 
-#         for i in range (len(coins)):
-#                 #### yha ab me again function calling kar dunga 
+        
+        not_pick=0+helper(coins,index-1,amount,dp)
+        
+        pick=(10**9)+7
+        
+        if coins[index]<=amount:
+                pick=1+helper(coins,index,amount-coins[index],dp)
                 
-#                 small_output=solve_recursion(coins,amount-coins[i])
-                
-#                 ### anb mai mere small_output ko check kar kar leta h 
-                
-#                 if small_output!=float('inf'):
-                        
-#                         mini=min(mini,1+small_output)
-                        
-#         return mini 
-
-def solve_rec_dp(coins,amount,dp):
+        dp[index][amount]= min(pick,not_pick)
+        return dp[index][amount]
         
-        if amount==0:
-                return 0
-        if amount<0:
-                return float('inf')
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
         
-        if dp[amount]!=-1:
-                
-                return dp[amount]
+        n=len(coins)
         
-        mini=float('inf')
+        dp=[[-1 for i in range (amount+1)] for j in range (n+1)]
         
-        for i in range (len(coins)):
-                
-                small_output=solve_rec_dp(coins,amount-coins[i],dp)
-                
-                if small_output!=float('inf'):
-                        
-                        mini=min(mini,1+small_output)
-                        
-        dp[amount]=mini
-        return dp[amount]
+        ans=helper(coins,n-1,amount,dp)
         
-        
-
-                        
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        # output=solve_recursion(coins,amount)
-        # if output==float('inf'):
-        #         return -1
-        # return output 
-        
-        ### recursion se delh liya ab memo use karenge 
-        
-        dp=[-1 for i in range (amount+1)]
-        
-        ans=solve_rec_dp(coins,amount,dp)
-        
-        if ans==float('inf'):   #### matlab mere amount given coin se banna possible nhi hai 
+        if ans==(10**9)+7:
                 return -1
+        else:
+                return ans 
         
-        return ans 
-                
